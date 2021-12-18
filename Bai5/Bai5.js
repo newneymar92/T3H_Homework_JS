917f600349ae2282238b5f1356ebe3ad8851bfa3
newneymar92 <nguyenanhtai.hust@gmail.com>
2021-12-19 00:50:22 +0700

    test2

--- Bai5/Bai5.js
+++ Bai5/Bai5.js
@@ -1,15 +1,14 @@
 // 5. Viết chương trình tính tổng từ 1 đến n?
 // VD: n = 10, output: 55
-
+//test
 let n = 10;
 
-function sum1toN(n){
-    let sum =0;
-    for(let i =0;i<=n;i++){
-        sum+=i;
-    }
-    console.log(sum);
+function sum1toN(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  console.log(sum);
 }
 
 sum1toN(n);
-
